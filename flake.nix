82b8c86a8818f1b3cceea28ffb4fc3ffafee8d98
Sam Title <samstitle@gmail.com>
2025-10-25 00:41:16 -0400

    chore: better formatting and highlighting

--- flake.nix
+++ flake.nix
@@ -2,14 +2,26 @@
   description = "Nix flake quine";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: {
-    apps.default = {
-      type = "app";
-      program = toString (
-        nixpkgs.legacyPackages.${system}.writeShellScript "quine" ''
-          cat ${builtins.path { path = ./flake.nix; name = "quine-source"; }}
-        ''
-      );
-    };
-  });
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      apps.default = {
+        type = "app";
+        program = toString (
+          nixpkgs.legacyPackages.${system}.writeShellScript "quine" ''
+            cat ${
+              builtins.path {
+                path = ./flake.nix;
+                name = "quine-source";
+              }
+            } \
+              | ${nixpkgs.legacyPackages.${system}.nixfmt-rfc-style}/bin/nixfmt \
+              | ${
+                nixpkgs.legacyPackages.${system}.bat
+              }/bin/bat --language=nix --style=plain --paging=never --color=always
+          ''
+        );
+      };
+      formatter = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+    });
 }
